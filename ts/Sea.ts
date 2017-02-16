70ad9f802b7590aef79dbbec7966c912001464c3
driftingPixel <bob.grabinski@gmail.com>
2017-02-16 18:56:56 +0100

    SEA Waves

--- ts/Sea.ts
+++ ts/Sea.ts
@@ -1,30 +1,46 @@
 class Sea{
 
     mesh: THREE.Mesh;
+	waves:any[];
 
     constructor(x?:number, y?:number, z?:number){
-        // create the geometry (shape) of the cylinder;
-	    // the parameters are: 
-	    // radius top, radius bottom, height, number of segments on the radius, number of segments vertically
-        let geom = new THREE.CylinderGeometry(600,600,800,40,10);
-        
-        // rotate the geometry on the x axis
-	    geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
+        var geom = new THREE.CylinderGeometry(600,600,800,40,10);
+	geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
 
-        // create the material 
-	    var mat = new THREE.MeshPhongMaterial({
-		    color: Colors.BLUE,
-		    transparent:true,
-		    opacity:.6,
-		    shading:THREE.FlatShading,
-	    });
+	// important: by merging vertices we ensure the continuity of the waves
+	geom.mergeVertices();
 
-        // To create an object in Three.js, we have to create a mesh 
-	    // which is a combination of a geometry and some material
-	    this.mesh = new THREE.Mesh(geom, mat);
+	// get the vertices
+	var l = geom.vertices.length;
 
-	    // Allow the sea to receive shadows
-	    this.mesh.receiveShadow = true; 
+	// create an array to store new data associated to each vertex
+	this.waves = new Array();
+
+	for (var i=0; i<l; i++){
+		// get each vertex
+		var v = geom.vertices[i];
+
+		// store some data associated to it
+		this.waves.push({y:v.y,
+										 x:v.x,
+										 z:v.z,
+										 // a random angle
+										 ang:Math.random()*Math.PI*2,
+										 // a random distance
+										 amp:5 + Math.random()*15,
+										 // a random speed between 0.016 and 0.048 radians / frame
+										 speed:0.016 + Math.random()*0.032
+										});
+	};
+	var mat = new THREE.MeshPhongMaterial({
+		color:Colors.BLUE,
+		transparent:true,
+		opacity:.8,
+		shading:THREE.FlatShading,
+	});
+
+	this.mesh = new THREE.Mesh(geom, mat);
+	this.mesh.receiveShadow = true;
 
         this.mesh.position.x = x!=undefined ? x:0;
         this.mesh.position.y = y!=undefined ? y:0;
@@ -32,6 +48,31 @@ class Sea{
     }
 
 	public update(){
+		// get the vertices
+		var verts = (<THREE.PolyhedronGeometry>this.mesh.geometry).vertices;
+		var l = verts.length;
+
+		for (var i=0; i<l; i++){
+			var v = verts[i];
+
+			// get the data associated to it
+			var vprops = this.waves[i];
+
+			// update the position of the vertex
+			v.x = vprops.x + Math.cos(vprops.ang)*vprops.amp;
+			v.y = vprops.y + Math.sin(vprops.ang)*vprops.amp;
+
+			// increment the angle for the next frame
+			vprops.ang += vprops.speed;
+
+		}
+
+		// Tell the renderer that the geometry of the sea has changed.
+		// In fact, in order to maintain the best level of performance, 
+		// three.js caches the geometries and ignores any changes
+		// unless we add this line
+		(<THREE.PolyhedronGeometry>this.mesh.geometry).verticesNeedUpdate=true;
+
 		this.mesh.rotation.z += .005;
 	}
 }
